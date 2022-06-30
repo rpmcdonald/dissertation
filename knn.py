d1ea4027a12871671c972082152584a266716646
Ross McDonald <rosspmcd@gmail.com>
2022-06-30 14:56:13 +0100

    added key frames, working on k-means and LDA

--- knn.py
+++ knn.py
@@ -10,7 +10,7 @@ data = "ssa"
 #data = "moz"
 mfcc_shape = 39
 length = 256
-n_components = 2
+n_components = 3
 pca = True
 
 if pca:
--- ssa_data_knn.py
+++ ssa_data_knn.py
@@ -11,9 +11,11 @@ import sklearn.preprocessing
 import pydub
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA, KernelPCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from scipy.cluster.vq import whiten
+from sklearn.cluster import KMeans
 import speechpy
 from tqdm import tqdm
 import os
@@ -27,13 +29,12 @@ def clean_df(file):
     for accent in ACCENTS:
         output_df = pd.concat([output_df, df[(df['country']==accent[0]) & (df['native_language']==accent[1])]], ignore_index=True)
     output_df.drop(['age', 'age_onset', 'birthplace', 'sex', 'speakerid', 'file_missing?'], axis=1, inplace=True)
-    #output_df.rename(columns={"country": "accent"}, inplace=True)
     return output_df
 
 
 class Mfcc():
 
-    def __init__(self, df, accent, limit, test_size, mfcc_size, target_size, randomise):
+    def __init__(self, df, accent, limit, test_size, mfcc_size, target_size, randomise, get_key_frames, k_means):
         self.df = df
         self.accent = accent
         self.col = "filename"
@@ -42,6 +43,8 @@ class Mfcc():
         self.mfcc_size = mfcc_size
         self.test_size = test_size
         self.randomise = randomise
+        self.gkf = get_key_frames
+        self.k_means = k_means
         self.names = []
 
     def mp3towav(self):
@@ -49,17 +52,64 @@ class Mfcc():
         for filename in tqdm(accent_df[self.col]):
             pydub.AudioSegment.from_mp3(f"../experiments_data/ssa/recordings/{filename}.mp3").export(f"../experiments_data/ssa/wavs/{self.accent[0]}_{self.accent[1]}/{filename}.wav", format="wav")
 
+    def get_key_frames(self, mfcc, delta, d_delta, rms, n):
+        rms = rms.reshape(-1)
+        values = list(zip(*sorted( [(x,i) for (i,x) in enumerate(rms)], 
+                    reverse=True )[:n*2] ))[0]
+        posns = list(zip(*sorted( [(x,i) for (i,x) in enumerate(rms)], 
+                    reverse=True )[:n*2] ))[1] 
+        #print(values)
+        #print(posns)
+        # Make sure highest values are not next to each other
+        cleaned_pos = []
+        for i in posns:
+            if len(cleaned_pos) == n:
+                break
+            for j in cleaned_pos:
+                if i == j+1 or i == j-1:
+                    break
+            else:
+                cleaned_pos.append(i)
+        #print(cleaned_pos)
+        return_mfcc = []
+        return_delta = []
+        return_d_delta = []
+        for i in range(len(mfcc)):
+            temp_mfcc = []
+            temp_delta = []
+            temp_d_delta = []
+            for n in cleaned_pos:
+                temp_mfcc.append(mfcc[i][n])
+                temp_delta.append(delta[i][n])
+                temp_d_delta.append(d_delta[i][n])
+            return_mfcc.append(temp_mfcc)
+            return_delta.append(temp_delta)
+            return_d_delta.append(temp_d_delta)
+
+        return np.array(return_mfcc), np.array(return_delta), np.array(return_d_delta)
+
+    def k_means_mfcc(self, mfcc):
+        kmeans = KMeans(n_clusters=3, random_state=0).fit_transform(mfcc)
+        print(kmeans)
+        sys.exit("End")
+
     def wavtomfcc(self, file_path):
         wave, sr = librosa.load(file_path, mono=True, sr=None)
-        n_fft=2048*2
-        hop_length=1024*2
-        win_length=2048*2
+        n_fft=2048
+        hop_length=512
+        win_length=2048
         # defaults n_fft=2048 hop_length=512 win_length=2048
         mfcc = librosa.feature.mfcc(y=wave, sr=sr, n_mfcc=13, win_length=win_length, hop_length=hop_length, n_fft=n_fft) # format is (n_mfcc, )
+        #sys.exit("End")
         delta = librosa.feature.delta(mfcc)
         d_delta = librosa.feature.delta(mfcc, order=2)
-        # print(mfcc.shape)
-        # sys.exit("End")
+        if self.gkf:
+            rms = librosa.feature.rms(y=wave)
+            #print(mfcc.shape, rms.shape)
+            mfcc, delta, d_delta = self.get_key_frames(mfcc, delta, d_delta, rms, 3)
+            #print(mfcc.shape)
+        # if self.k_means:
+        #     self.k_means_mfcc(mfcc)
         return mfcc, delta, d_delta
 
     def create_mfcc(self):
@@ -92,8 +142,12 @@ class Mfcc():
         combined = []
         for i in range(len(self.list_of_mfccs)):
             combined.append(np.concatenate((self.list_of_mfccs[i], self.list_of_deltas[i], self.list_of_d_deltas[i])))
-        resized = [librosa.util.fix_length(mfcc, size=self.target_size, axis=1)
-                         for mfcc in combined]
+        if not self.gkf:
+            resized = [librosa.util.fix_length(mfcc, size=self.target_size, axis=1) for mfcc in combined]
+            if self.k_means:
+                self.k_means_mfcc(combined)
+        else:
+            resized = [librosa.util.fix_length(mfcc, size=3, axis=1) for mfcc in combined]
         print("len of mfccs reshaped", len(self.list_of_mfccs))
         print("Single mfcc reshaped shape:", resized[0].shape)
         
@@ -107,10 +161,17 @@ class Mfcc():
             X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, stratify=self.y, shuffle = True, test_size=self.test_size)
         else:
             X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, shuffle = False, test_size=self.test_size)
-        self.X_train = np.array(X_train).reshape(-1, self.mfcc_size, self.target_size)
-        print("X_train shape", self.X_train.shape)
-        self.X_test = np.array(X_test).reshape(-1, self.mfcc_size, self.target_size)
-        print("X_test shape", self.X_test.shape)
+        
+        if not self.gkf:
+            self.X_train = np.array(X_train).reshape(-1, self.mfcc_size, self.target_size)
+            print("X_train shape", self.X_train.shape)
+            self.X_test = np.array(X_test).reshape(-1, self.mfcc_size, self.target_size)
+            print("X_test shape", self.X_test.shape)
+        else:
+            self.X_train = np.array(X_train).reshape(-1, self.mfcc_size, 3)
+            print("X_train shape", self.X_train.shape)
+            self.X_test = np.array(X_test).reshape(-1, self.mfcc_size, 3)
+            print("X_test shape", self.X_test.shape)
         self.y_train = np.array(y_train).reshape(-1, 1)
         self.y_test = np.array(y_test).reshape(-1,1)
 
@@ -138,15 +199,25 @@ if __name__ == '__main__':
     names = []
     target_size=256
     mfcc_size=39
-    run_pca = True
-    run_new_pca = False
+    run_pca = False
+    run_lda = True
     randomise = False
+    get_key_frames = False
+    k_means = False
     if randomise == False:
         random.seed(1)
 
     for accent in ACCENTS:
         print(accent)
-        mfcc = Mfcc(df=df, accent=accent, limit=30, test_size=6, target_size=target_size, mfcc_size=mfcc_size, randomise=randomise)
+        mfcc = Mfcc(df=df, 
+                    accent=accent, 
+                    limit=30, 
+                    test_size=6, 
+                    target_size=target_size, 
+                    mfcc_size=mfcc_size, 
+                    randomise=randomise, 
+                    get_key_frames=get_key_frames,
+                    k_means=k_means)
         # mfcc.mp3towav()
         mfcc.create_mfcc()
         mfcc.resize_mfcc()
@@ -173,16 +244,7 @@ if __name__ == '__main__':
     X_train_std=whiten(X_train.transpose()).transpose()
     X_test_std=whiten(X_test.transpose()).transpose()
 
-    # DIFFERENT METHOD NOT WORKING
-    # avgVal=np.mean(X_train,1) 
-    # cmbAvg=X_train-avgVal[:,None] 
-    # prcAbs=np.percentile(np.abs(cmbAvg),95,1)/2 
-    # combinedDelta2Norm=cmbAvg/prcAbs[:,None] 
-    # std2=np.std(combinedDelta2Norm,1)
-    # print(X_train)
-    # print(std2.shape)
-
-
+    # Visualise standarised data
     # y = []
     # for x in X_train_std[0]:
     #     for i in x:
@@ -197,22 +259,15 @@ if __name__ == '__main__':
         pca = PCA(n_components=3)
         #pipe = Pipeline([('scaler', StandardScaler()), ('pca', pca)]) # Can add this instead of pca below, gives different results and don't know why
 
-        kpca = KernelPCA(n_components = 2)
-
         nsamples, nx, ny = X_train_std.shape
         X_train_reshape = X_train_std.reshape((nsamples,nx*ny))
         x_train_pca = pca.fit_transform(X_train_reshape)
         print(X_train_std.shape, X_train_reshape.shape, x_train_pca.shape)
-        #print(x_train_pca[0])
-        #x_train_pca = np.array(x_train_pca).reshape(-1, mfcc_size, target_size)
-        # print(self.X_train_std.shape, x_train_pca.shape)
-        # print(self.X_train_std[0][0][0], x_train_pca[0][0][0])
         X_train_std = x_train_pca
 
         nsamples, nx, ny = X_test_std.shape
         X_test_reshape = X_test_std.reshape((nsamples,nx*ny))
         x_test_pca = pca.transform(X_test_reshape)
-        #x_test_pca = np.array(x_test_pca).reshape(-1, mfcc_size, target_size)
         X_test_std = x_test_pca
 
         fig = plt.figure()
@@ -235,42 +290,16 @@ if __name__ == '__main__':
 
         # interactive_3d_plot(x_train_pca, names)
 
-    if run_new_pca:
-        print("in PCA")
-        pca = PCA(n_components=2)
-        #pipe = Pipeline([('scaler', StandardScaler()), ('pca', pca)])
-        x_train_pca = []
-        for x in X_train_std:
-            x_train_pca.append(pca.fit_transform(x.T))
-        x_test_pca = []
-        for x in X_test_std:
-            x_test_pca.append(pca.transform(x.T))
-        X_train_std = x_train_pca
-        X_test_std = x_test_pca
-        
-        print(len(x_train_pca),len(x_train_pca[0]),len(x_train_pca[0][0]))
-        print(x_train_pca[0][0][0])
+    # --- LDA
+    if run_lda:
+        print("in LDA")
+        lda = LinearDiscriminantAnalysis()
+        nsamples, nx, ny = X_train_std.shape
+        X_train_reshape = X_train_std.reshape((nsamples,nx*ny))
+        lda.fit(X_train_reshape, y_train.reshape(-1))
+        transformed = lda.transform(X_train_std)
+        print(transformed.shape)
 
-        # fig = plt.figure()
-        # ax = fig.add_subplot(projection='3d')
-        # scatter = ax.scatter(x_train_pca[:,0], x_train_pca[:,1], x_train_pca[:,2], c=y_train)
-        # legend1 = ax.legend(*scatter.legend_elements(),
-        #             loc="lower left", title="Classes")
-        # ax.add_artist(legend1)
-        fig = plt.figure()
-        plt.scatter(x_train_pca[0][:,0], x_train_pca[0][:,1])
-        plt.show()
-
-        # fig = plt.figure()
-        # ax = fig.add_subplot(projection='3d')
-        # # ax.scatter(x_train_pca[0][:,0], x_train_pca[0][:,1], x_train_pca[0][:,2])
-        # # ax.scatter(x_train_pca[7][:,0], x_train_pca[7][:,1], x_train_pca[7][:,2])
-        # # ax.scatter(x_train_pca[51][:,0], x_train_pca[51][:,1], x_train_pca[51][:,2])
-        # # ax.scatter(x_train_pca[52][:,0], x_train_pca[52][:,1], x_train_pca[52][:,2])
-        # colors = ["red", "blue", "green"]
-        # for i in range(len(x_train_pca)):
-        #     ax.scatter(x_train_pca[i][:,0], x_train_pca[i][:,1], x_train_pca[i][:,2], color=colors[y_train[i][0]])
-        # plt.show()
 
     np.save(f'mfccs/X_train_ssa.npy', X_train_std)
     np.save(f'mfccs/X_test_ssa.npy', X_test_std)
