103f66813eb041b6da08043539f43a1947d02c56
Ross McDonald <rosspmcd@gmail.com>
2022-07-06 11:39:43 +0100

    svm set up

--- svm.py
+++ svm.py
@@ -21,33 +21,40 @@ y_test = np.load(f'mfccs/y_test_{data}.npy')
 
 print(X_train.shape, X_test.shape, len(y_train), len(y_test))
 
-# parameters = [{'C' : [1,10,100,1000], 'kernel' : ['linear']},{'C' : [1,10,100,1000], 'kernel' : ['rbf'], 'gamma' : [0.5,0.1,0.01,0.001]}]
-# classifier = SVC(kernel = 'linear', random_state=0,  C=1)
-# grid_search = GridSearchCV(estimator=classifier,
-# 	param_grid=parameters,
-# 	scoring='accuracy',
-# 	cv=10,
-# 	n_jobs=-1)
-# grid_search = grid_search.fit(X_train,y_train)
-# best_accuracy = grid_search.best_score_
-# print('\nBest Accuracy : \n{}'.format(best_accuracy))
-# best_parameters = grid_search.best_params_
-# print('\nBest Parameters : \n{}'.format(best_parameters))
+# Set up model parameters
+parameters = [{'C' : [1,10,100,1000], 'kernel' : ['linear']},{'C' : [1,10,100,1000], 'kernel' : ['rbf'], 'gamma' : [0.5,0.1,0.01,0.001]}]
+classifier = SVC(kernel = 'linear', random_state=0,  C=1)
 
-model = SVC(kernel = 'rbf', probability=True)
+grid_search = GridSearchCV(estimator=classifier,
+	param_grid=parameters,
+	scoring='accuracy',
+	cv=10,
+	n_jobs=-1)
 
 nsamples, nx, ny = X_train.shape
 X_train_reshape = X_train.reshape((nsamples,nx*ny))
+y_train = np.ravel(y_train)
+
+# Find optimal model
+grid_search = grid_search.fit(X_train_reshape,y_train)
+best_accuracy = grid_search.best_score_
+print('\nBest Accuracy : \n{}'.format(best_accuracy))
+best_parameters = grid_search.best_params_
+print('\nBest Parameters : \n{}'.format(best_parameters))
+
+# Run optimal model on test data
+if best_parameters["kernel"] == "linear":
+    model = SVC(kernel = best_parameters["kernel"], C = best_parameters["C"], probability=True)
+else:
+    model = SVC(kernel = best_parameters["kernel"], C = best_parameters["C"], gamma = best_parameters["gamma"], probability=True)
+model.fit(X_train_reshape, y_train)
 
 nsamples, nx, ny = X_test.shape
 X_test_reshape = X_test.reshape((nsamples,nx*ny))
-
-model.fit(X_train_reshape, y_train)
 
 y_predict = model.predict(X_test_reshape)
 y_test = np.ravel(y_test)
 
 print(f'Model Score: {model.score(X_test_reshape, y_test)}')
-#print(f"Best params: {model.best_params_}")
 cm = confusion_matrix(y_test, y_predict)
 print(f'Confusion Matrix: \n{cm}')
