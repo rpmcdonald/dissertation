a2c7e634f5943dea202301a9321e0a8625903350
Ross McDonald <rosspmcd@gmail.com>
2022-07-07 22:17:21 +0100

    random forest, added grid search

--- rfc.py
+++ rfc.py
@@ -20,7 +20,22 @@ y_train = np.load(f'mfccs/y_train_{data}.npy')
 y_test = np.load(f'mfccs/y_test_{data}.npy')
 
 # Initialise and fit
-model = RandomForestClassifier(n_estimators = 150)
+parameters = { 
+    'n_estimators': [100, 200, 500],
+    'max_features': ['sqrt', 'log2'],
+    #'max_depth' : [4,5,6,7,8],
+    'criterion' :['gini', 'entropy']
+}
+classifier = RandomForestClassifier()
+
+model = GridSearchCV(estimator=classifier,
+	param_grid=parameters,
+	scoring='accuracy',
+	cv=5,
+	n_jobs=-1, 
+    verbose=2)
+
+#model = RandomForestClassifier(n_estimators = 150)
 
 if not pca:
     nsamples, nx, ny = X_train.shape
@@ -42,6 +57,7 @@ else:
 y_predict = model.predict(X_test_reshape)
 y_test = np.ravel(y_test)
 print(f'Model Score: {model.score(X_test_reshape, y_test)}')
+print(f"Best params: {model.best_params_}")
 cm = confusion_matrix(y_test, y_predict)
 print(f'Confusion Matrix: \n{cm}')
 
