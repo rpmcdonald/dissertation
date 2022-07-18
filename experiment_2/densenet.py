0a184d5ee28fb5cc2aebef73eaa50f52f842e93f
Ross McDonald <rosspmcd@gmail.com>
2022-07-18 16:45:00 +0100

    working on setting up densenet

--- experiment_2/densenet.py
+++ experiment_2/densenet.py
@@ -1,42 +1,38 @@
-from keras.models import load_model
-import numpy as np
-from keras.callbacks import TensorBoard
-from keras.callbacks import Callback
+from xml.etree.ElementInclude import include
 import tensorflow as tf
-import keras.backend as K
+from tensorflow import keras
+from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Activation, Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.applications import DenseNet121, DenseNet169
+
+import numpy as np
 import matplotlib.pyplot as plt
-import librosa
-import librosa.display
-import IPython.display
-import sklearn.preprocessing
-import pydub
-from keras.utils import to_categorical
-import keras
-from keras.models import Sequential
-from keras.layers import Activation, Dense, Dropout, Flatten, Conv2D, MaxPooling2D
 
-# model = load_model("../models/final_model_3.h5")
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  # Restrict TensorFlow to only allocate 2GB of memory on the first GPU
+  try:
+    tf.config.experimental.set_virtual_device_configuration(
+        gpus[0],
+        [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=2048)])
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Virtual devices must be set before GPUs have been initialized
+    print(e)
 
-data = "moz"
-spectrogram_shape = 128
+mfcc_shape = 39
 length = 256
 classes = 2
 
-# X_train = np.load(f"spectrograms/X_train_{data}.npy").reshape(-1, spectrogram_shape, length, 1)
-# X_test = np.load(f"spectrograms/X_test_{data}.npy").reshape(-1, spectrogram_shape, length, 1)
-# X_val = np.load(f"spectrograms/X_val_{data}.npy").reshape(-1, spectrogram_shape, length, 1)
-# y_train = np.load(f"spectrograms/y_train_{data}.npy")
-# y_test = np.load(f"spectrograms/y_test_{data}.npy")
-# y_val = np.load(f"spectrograms/y_val_{data}.npy")
-
-X_train = np.load("spectrograms/X_train_moz.npy").reshape(-1, spectrogram_shape, length, 1)
-X_test = np.load("spectrograms/X_test_moz.npy").reshape(-1, spectrogram_shape, length, 1)
-X_val = np.load("spectrograms/X_val_moz.npy").reshape(-1, spectrogram_shape, length, 1)
-y_train = np.load("spectrograms/y_train_moz.npy")
-y_test = np.load("spectrograms/y_test_moz.npy")
-y_val = np.load("spectrograms/y_val_moz.npy")
-
-print(X_train.shape, X_test.shape, X_val.shape, len(y_train), len(y_test), len(y_val))
+X_train = np.load("mfccs/X_train_moz.npy").reshape(-1, mfcc_shape, length, 1)
+X_test = np.load("mfccs/X_test_moz.npy").reshape(-1, mfcc_shape, length, 1)
+X_val = np.load("mfccs/X_val_moz.npy").reshape(-1, mfcc_shape, length, 1)
+y_train = np.load("mfccs/y_train_moz.npy")
+y_test = np.load("mfccs/y_test_moz.npy")
+y_val = np.load("mfccs/y_val_moz.npy")
 
 y_train_hot = to_categorical(y_train, num_classes=classes)
 y_test_hot = to_categorical(y_test, num_classes=classes)
@@ -45,30 +41,24 @@ y_val_hot = to_categorical(y_val, num_classes=classes)
 callbacks = [TensorBoard(log_dir="./logs")]
 
 model = Sequential()
-model.add(Conv2D(16, (3, 3), input_shape=(spectrogram_shape, length, 1)))
-model.add(Activation("relu"))
-model.add(Conv2D(16, (3, 3)))
-model.add(Activation("relu"))
+model.add(Conv2D(16, (7, 7), input_shape=(mfcc_shape, length, 1)))
 model.add(MaxPooling2D(pool_size=(3, 3)))
-model.add(Dropout(0.5))
+model.add(Dense(classes*8, activation="relu"))
 
-model.add(Conv2D(16, (3, 3)))
-model.add(Activation("relu"))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Dropout(0.15))
-
-model.add(Conv2D(32, (3, 3)))
-model.add(Activation("relu"))
+model.add(Conv2D(16, (1, 1)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-# the model so far outputs 3D feature maps (height, width, features)
+model.add(Dense(classes*4, activation="relu"))
 
-model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
-model.add(Dense(32))
-model.add(Activation("relu"))
-model.add(Dropout(0.5))
-model.add(Dense(classes))
-model.add(Activation("softmax"))
+model.add(Conv2D(16, (1, 1)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dense(classes*2, activation="relu"))
+
+model.add(Conv2D(16, (1, 1)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(classes, activation="relu"))
+model.add(Activation('softmax'))
 
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adagrad(lr=0.01),
@@ -76,7 +66,7 @@ model.compile(loss=keras.losses.categorical_crossentropy,
 
 print(X_train.shape, y_train_hot.shape, X_val.shape, y_val_hot.shape)
 
-history = model.fit(X_train, y_train_hot, batch_size=128, epochs=50, verbose=1,
+history = model.fit(X_train, y_train_hot, batch_size=128, epochs=150, verbose=1,
             validation_data=(X_val, y_val_hot), callbacks=callbacks)
 
 
